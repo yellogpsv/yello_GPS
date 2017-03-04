26a1db69c61d079804a35d919989658bcb1189cf
YelloSOFT <yellosoft@mail.ru>
2017-03-04 09:45:08 +0300

    Initial commit

---
+++ app/src/main/java/ru/yellosoft_club/yello_gps/MainActivity.java
@@ -0,0 +1,25 @@
+package ru.yellosoft_club.yello_gps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    private void testup_GITHUB()
+    {
+        int i =1+1;
+    }
+
+    }
+
+
